7ab877b6b42f20436f114ae04590653dbd247645
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2019-08-12 19:11:06 +0200

    Finished counter react classes

--- src/AppClass.js
+++ src/AppClass.js
@@ -3,10 +3,33 @@ import './App.css';
 import './profile/Profile.css';
 
 export default class AppClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 1 };
+    this.onIncrementClick = this.onIncrementClick.bind(this);
+    this.onDecrementClick = this.onDecrementClick.bind(this);
+  }
+
+  onIncrementClick() {
+    this.setState(prevState => ({ counter: prevState.counter + 1 }));
+  }
+
+  onDecrementClick() {
+    this.setState(prevState => ({ counter: prevState.counter - 1 }));
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello Euricom dev cruise 2019!</h1>
+        <div className="counter">
+          <button className="box with-hover" onClick={this.onIncrementClick}>
+            +
+          </button>
+          <span>{this.state.counter}</span>
+          <button className="box with-hover" onClick={this.onDecrementClick}>
+            -
+          </button>
+        </div>
       </div>
     );
   }
