c1d4c960e081f542a94b4d7db0033e6b5ae764ac
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2019-08-12 19:15:43 +0200

    Finished counter useState

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,36 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './profile/Profile.css';
 
-export default class AppClass extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 1 };
-    this.onIncrementClick = this.onIncrementClick.bind(this);
-    this.onDecrementClick = this.onDecrementClick.bind(this);
-  }
-
-  onIncrementClick() {
-    this.setState(prevState => ({ counter: prevState.counter + 1 }));
-  }
-
-  onDecrementClick() {
-    this.setState(prevState => ({ counter: prevState.counter - 1 }));
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="counter">
-          <button className="box with-hover" onClick={this.onIncrementClick}>
-            +
-          </button>
-          <span>{this.state.counter}</span>
-          <button className="box with-hover" onClick={this.onDecrementClick}>
-            -
-          </button>
-        </div>
+export default function AppClass() {
+  const [counter, setCounter] = useState(1);
+  return (
+    <div className="App">
+      <div className="counter">
+        <button
+          className="box with-hover"
+          onClick={() => setCounter(counter + 1)}
+        >
+          +
+        </button>
+        <span>{counter}</span>
+        <button
+          className="box with-hover"
+          onClick={() => setCounter(counter - 1)}
+        >
+          -
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
