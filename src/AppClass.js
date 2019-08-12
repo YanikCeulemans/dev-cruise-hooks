3964ad26fcea4b098034726d2be5a861399f431e
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2019-08-12 19:16:58 +0200

    Finished counter useState re-use

--- src/AppClass.js
+++ src/AppClass.js
@@ -2,22 +2,25 @@ import React, { useState } from 'react';
 import './App.css';
 import './profile/Profile.css';
 
+function useCounter(initialValue) {
+  const [counter, setCounter] = useState(initialValue);
+
+  const increment = () => setCounter(counter + 1);
+  const decrement = () => setCounter(counter - 1);
+
+  return [counter, increment, decrement];
+}
+
 export default function AppClass() {
-  const [counter, setCounter] = useState(1);
+  const [counter, increment, decrement] = useCounter(1);
   return (
     <div className="App">
       <div className="counter">
-        <button
-          className="box with-hover"
-          onClick={() => setCounter(counter + 1)}
-        >
+        <button className="box with-hover" onClick={increment}>
           +
         </button>
         <span>{counter}</span>
-        <button
-          className="box with-hover"
-          onClick={() => setCounter(counter - 1)}
-        >
+        <button className="box with-hover" onClick={decrement}>
           -
         </button>
       </div>
